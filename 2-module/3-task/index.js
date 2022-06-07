c6c323ec8c512265a6a20742caa922339319c3fc
NikitaFedorov <nfedorov15@yandex.ru>
2022-06-07 23:50:30 +0500

    2module

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,4 +1,10 @@
 let calculator = {
+  
+  read() {
+    this.a = +prompt ("Введите первое число a:", 5);
+    this.b = +prompt ("Введите второе число b:", 3);
+  },
+  
   sum() {
     return this.a + this.b;
   },
@@ -6,16 +12,10 @@ let calculator = {
   mul() {
     return this.a * this.b;
   },
-
-  read() {
-    this.a = +prompt('a?', 0);
-    this.b = +prompt('b?', 0);
-  }
 };
-
-calculator.read(3, 5);
-console.log(calculator.sum()); 
-console.log(calculator.mul());
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
