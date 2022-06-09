21155b76a7e80e25d7e30157d2fa2edcdd4afb3f
NikitaFedorov <nfedorov15@yandex.ru>
2022-06-09 22:40:23 +0500

    2module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,4 +1,4 @@
-function sumSalary(salaries) {
+
   let salaries = {
     John: 1000,
     Ann: 1600,
@@ -7,26 +7,20 @@ function sumSalary(salaries) {
     currency: 'USD',
     isPayed: false
   }
-    
-  function sumSalaries (salaries){
-    
-    
+
+  function sumSalariy (salaries){
   let sum = 0; 
   for (let key in salaries){
-  }
-  for (typeof salaries[key] == 'number'){
-  }
-  for (isFinite(salaries[key])){
-  }
-  for (!Number.isNaN(salaries[key])){
-           
-    sum += salaries[key];
+    if (typeof salaries[key] == 'number'){
+      if (isFinite(salaries[key])){
+          if (!Number.isNaN(salaries[key])){
+            sum += salaries[key];
   }
   }
-}
-  
+  }
+  }
   return (sum);
-    
-  
+    }
+
     sumSalaries(salaries);
 
