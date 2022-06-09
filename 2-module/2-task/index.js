1985fdb83f60557129584dde492ac9596f58e5de
NikitaFedorov <nfedorov15@yandex.ru>
2022-06-09 23:28:13 +0500

    2module

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,6 +1,15 @@
 function isEmpty(obj) {
-  for (let key in obj){
-    return true
-  }
-  return false
+  for (let key in obj) {
+  
+  return true;
 }
+return false;
+}
+
+let schedule = {};
+
+alert( isEmpty(schedule) ); 
+
+schedule["8:30"] = "подъём";
+
+alert( isEmpty(schedule) ); 
